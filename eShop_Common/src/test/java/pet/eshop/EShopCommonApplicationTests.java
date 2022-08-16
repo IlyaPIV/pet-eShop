0bd71940311b74f560c6d087e20afa52f310d8c5
IlyaPIV <petermoloch@mail.ru>
2022-08-16 15:23:05 +0300

    #1 - base structure of project is created

---
+++ eShop_Common/src/test/java/pet/eshop/EShopCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
