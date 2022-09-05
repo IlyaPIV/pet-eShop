4c6099fa6f934dcf0b692da88a96f99fa5109bd9
IlyaPIV <petermoloch@mail.ru>
2022-09-05 11:00:22 +0300

    #7 - New user functional

--- eShop_WebParent/eShop_BackEnd/src/main/java/pet/eshop/admin/users/UserController.java
+++ eShop_WebParent/eShop_BackEnd/src/main/java/pet/eshop/admin/users/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pet.eshop.common.entity.User;
 
 import java.util.List;
@@ -20,5 +22,22 @@ public class UserController {
         model.addAttribute("listUsers", listUsers);
 
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model){
+        User newUser = new User();
+        newUser.setEnabled(true);
+        model.addAttribute("newUser", newUser);
+        model.addAttribute("listRoles", service.listRoles());
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes){
+        System.out.println(user);
+        service.save(user);
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+        return "redirect:/users";
     }
 }
--- eShop_WebParent/eShop_BackEnd/src/main/java/pet/eshop/admin/users/UserService.java
+++ eShop_WebParent/eShop_BackEnd/src/main/java/pet/eshop/admin/users/UserService.java
@@ -2,6 +2,7 @@ package pet.eshop.admin.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pet.eshop.common.entity.Role;
 import pet.eshop.common.entity.User;
 
 import java.util.List;
@@ -10,9 +11,20 @@ import java.util.List;
 public class UserService {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepo;
+
+    @Autowired
+    private RoleRepository roleRepo;
 
     public List<User> listAll(){
-        return (List<User>) repo.findAll();
+        return (List<User>) userRepo.findAll();
+    }
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepo.findAll();
+    }
+
+    public void save(User user) {
+        userRepo.save(user);
     }
 }
