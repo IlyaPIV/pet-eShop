08be7fdde71da6d5d23793e4bd611fe36c3082f3
IlyaPIV <petermoloch@mail.ru>
2022-09-09 20:34:23 +0300

    #14 - uploading user photos

--- eShop_Common/src/main/java/pet/eshop/common/entity/User.java
+++ eShop_Common/src/main/java/pet/eshop/common/entity/User.java
@@ -132,4 +132,11 @@ public class User {
         this.roles.add(role);
     }
 
+
+    @Transient
+    public String getPhotosImagePath(){
+        if (id == null || photos == null) return "/images/default-user.png";
+        return "/user-photos/" + this.id + "/" + this.photos;
+    }
+
 }
