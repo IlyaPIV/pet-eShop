5d3201722ef6199345deece32b513f66213c67ea
IlyaPIV <petermoloch@mail.ru>
2022-09-12 22:27:53 +0300

    #18 - Updating html pages (adaptation for different resolution) + refactoring html

--- eShop_Common/src/main/java/pet/eshop/common/entity/User.java
+++ eShop_Common/src/main/java/pet/eshop/common/entity/User.java
@@ -139,4 +139,8 @@ public class User {
         return "/user-photos/" + this.id + "/" + this.photos;
     }
 
+    @Transient
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
 }
